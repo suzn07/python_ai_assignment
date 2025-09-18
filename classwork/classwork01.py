fabcbe57b561f52a58177f3afc6acec65135e2e5
suzn077 <baralisujan101@gmail.com>
2025-09-18 10:28:14 +0300

    classwork

--- classwork/classwork01.py
+++ classwork/classwork01.py
@@ -1,3 +1,21 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# df = pd.read_csv('linreg_data.csv')
+#
+# # df = np.loadtxt("linreg_data.csv", delimiter=",")
+#
+# df = np.array(df)
+#
+# x = df[:, 0]
+# y = df[:, 1]
+#
+#
+# plt.scatter(x,y,color='red', marker='+')
+#
+# plt.show()
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
